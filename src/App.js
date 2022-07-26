cd67b9b636400508bdacec4c6409d29141b8155a
Rob <kritz.rob@gmail.com>
2022-07-26 23:36:05 +0200

    useCallbacks, backend, envs

--- src/App.js
+++ src/App.js
@@ -1,83 +1,50 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { useSearchParams } from 'react-router-dom';
 
 import axios from 'axios';
 
 import Modal from './components/modal'
 import useModal from './hooks/useModal'
+import { BACKEND_URI } from './config'
 import './App.css';
 
 function App() {
 
-  console.log('render')
-
+  console.log('render');
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const [selectedWallet, setSelectedWallet] = useState(null);
-
-  const [signError, setSignError] = useState(false);
-  const [posting, setPosting] = useState(false);
-  const [postError, setPostError] = useState(false);
   const [postSuccess, setPostSuccess] = useState(false);
+  const [postError, setPostError] = useState(false);
+  const [posting, setPosting] = useState(false);
+  const [signError, setSignError] = useState(false);
 
+  const [selectedWallet, setSelectedWallet] = useState(null);
   const [wallets, setWallets] = useState(null);
   const [cardano, setCardano] = useState(null);
 
   const { isShowing, toggle } = useModal();
 
-  useEffect(() => {
-    setCardano((typeof window !== "undefined") && window.cardano);
-  }, [])
-
-  useEffect(() => {
-    findWallets();
-  }, [cardano]);
-
-  const findWallets = () => {
-
+  const findWallets = useCallback(() => {
     const existingWallets = cardano ? Object.keys(cardano).filter(
       (walletName) =>
         walletName === "nami" ||
-        walletName === "ccvault" ||
         walletName === "eternl" ||
         walletName === "flint"
     ) : [];
 
     setWallets(existingWallets);
-  }
+  }, [cardano]);
 
-  const presentAuth = async () => {
-
-    console.log(selectedWallet);
-
-    try {
-
-      const api = await cardano[selectedWallet].enable();
-
-      const reward_addr = (await api.getRewardAddresses())[0];
-
-      const sig = await api.signData(reward_addr, searchParams.get('data'));
-      
-      post(sig, reward_addr);
-
-    } catch (e) {
-      console.error('Error during sig', e);
-      setSignError(true);
-    }
-
-  }
-
-  const post = async (sig, reward_addr) => {
-
+  const post = useCallback(async (sig, reward_addr) => {
     try {
       setPosting(true);
       setPostError(false);
 
       let result = await axios({
         method: "POST",
-        url: 'http://localhost:3080/api/users/walletregister',
+        url: BACKEND_URI + 'api/users/walletregister',
         headers: {
-          authorization: searchParams.get('token') || null
+          authorization: searchParams.get('auth') || null
         },
         data: {
           addr: reward_addr,
@@ -86,27 +53,50 @@ function App() {
         }
       })
 
-      if (result.statue == 200)
+      if (result.status === 200)
         setPostSuccess(true);
 
     } catch (e) {
       console.error(e);
       setPostError(true);
     }
-
     setPosting(false);
+  }, [searchParams]);
 
-  }
+  const presentAuth = useCallback(async () => {
+    try {
 
-  const connect = () => {
+      const api = await cardano[selectedWallet].enable();
+
+      const reward_addr = (await api.getRewardAddresses())[0];
+
+      const sig = await api.signData(reward_addr, searchParams.get('data'));
+
+      post(sig, reward_addr);
+
+    } catch (e) {
+      console.error('Error during sig', e);
+      setSignError(true);
+    }
+  }, [cardano, post, searchParams, selectedWallet]);
+
+  const connect = useCallback(() => {
     setSelectedWallet(null);
     toggle();
-  }
+  }, [toggle]);
 
-  const reset = () => {
+  const reset = useCallback(() => {
     toggle();
     setSignError(false);
-  }
+  }, [toggle]);
+
+  useEffect(() => {
+    setTimeout(setCardano((typeof window !== "undefined") && window.cardano), 50);
+  }, []);
+
+  useEffect(() => {
+    findWallets();
+  }, [cardano, findWallets]);
 
   return (
     <div>
